234fb5301cf6f33a2b6c2ca8ee8d2b091a25e4b6
ATOU11 <prince.atou@epitech.digital>
2024-10-02 20:59:15 +0200

    Create ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,12 @@
+// On déclare la fonction getHousePrototype
+function getHousesPrototype() {
+// housesPrototype est initialisée comme un objet de type Map pour stocker des clés uniques
+	const housesPrototype = new Map ([
+  	["Apartement 75008", 90],
+    ["Apartement 94300", 105],
+    ["Apartement 92300", 140],
+    ["Apartement 75015", 65]
+   ]);
+// On renvoie  le housesPrototype
+  return housesPrototype
+}
